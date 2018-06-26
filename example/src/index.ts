fec94d1d16c12729f693818c5572fe3b0fb09f29
Peter van der Noord <petervdn@mediamonks.com>
2018-06-26 13:47:21 +0200

    Add example folder (for testing during development)

---
+++ example/src/index.ts
@@ -0,0 +1,14 @@
+import MusicTime from '../../src/lib/MusicTime';
+
+['1.0.0', '1.2.3', '1.4.0', '21.0.12'].forEach(entry => {
+  const time = MusicTime.fromString(entry);
+  console.log(entry, time.getBBS(), time.beats, time.toString());
+});
+
+
+const t1 = new MusicTime(0,8,0);
+const t2 = new MusicTime(0,0,17);
+
+// expect(new MusicTime(0,8,0).add(new MusicTime(0,0,17)).toString()).to.equal('3.0.1');
+console.log(t1, t2);
+console.log(t2.add(t2));
